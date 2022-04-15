c5514b4cc3997e7d7a595ce8de653dfcafc58dc3
gacallea <gacallea@users.noreply.github.com>
2022-04-15 21:12:03 +0200

    this was great, flake8 changes only

--- linter.py
+++ linter.py
@@ -2,17 +2,17 @@ from SublimeLinter.lint import NodeLinter
 
 
 class TSStandard(NodeLinter):
-    name = 'ts-standard'
-    cmd = 'ts-standard --stdin --stdin-filename ${file}'
-    regex = r'^.+:(?P<line>\d+):(?P<col>\d+):\s*(?P<message>.+)'
+    name = "ts-standard"
+    cmd = "ts-standard --stdin --stdin-filename ${file}"
+    regex = r"^.+:(?P<line>\d+):(?P<col>\d+):\s*(?P<message>.+)"
     multiline = True
     defaults = {
-        'selector': 'source.ts, source.tsx',
-        'disable_if_not_dependency': False
+        "selector": "source.ts, source.tsx",
+        "disable_if_not_dependency": False,
     }
 
     def run(self, cmd, code):
-        if not self.context.get('file'):
+        if not self.context.get("file"):
             self.notify_failure()
-            return '-:1:1:The file must be saved before it can be linted by ts-standard'
+            return "-:1:1:The file must be saved before it can be linted by ts-standard"
         return super().run(cmd, code)
